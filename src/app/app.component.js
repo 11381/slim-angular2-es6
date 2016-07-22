eaf6ad8ffceb6d9248a48e93fa659d800a2dba85
Q <Q>
2016-07-21 20:37:07 -0700

    added some uncommitted entries

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "my-app",
-    template: "<h1>app with es6 whoa!</h1>"
+    template: "<h1>app with es6!</h1>"
 })
 export class AppComponent {
     constructor(){
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    entry: "./app/main.js",
+    entry: "./src/app/main.js",
     output: {
-        filename: "bundle.js"
+        filename: "./src/bundle.js"
     },
     module: {
         loaders: [
